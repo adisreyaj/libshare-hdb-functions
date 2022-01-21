8aa756c3d5755687453cec536f5aa8dc803c1c1e
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-01-21 23:57:24 +0530

    chore: add file watcher

---
+++ scripts/file-watcher.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+const chokidar = require('chokidar');
+require('dotenv').config();
+const updateFunctions = async () => {
+  try {
+    const res = fetch('http://localhost:9925', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Basic ${process.env.HDB_AUTH}`,
+      },
+      body: JSON.stringify({ operation: 'restart_service', service: 'custom_functions' }),
+    });
+    console.log('Custom Functions Updated');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+chokidar.watch('./api/**/*.js').on('change', (event, path) => {
+  updateFunctions();
+});
