6de756b757c4f0a92daaee19a1dc61b013ffbbce
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-01-22 00:39:05 +0530

    feat(libraries): add new and get library methods added

--- api/helpers/libraries.handler.js
+++ api/helpers/libraries.handler.js
@@ -1,13 +1,47 @@
 'use strict';
+const qb = require('./query-builder.helper');
 
 const getLibrariesHandler =
-  ({ hdbCore, logger }) =>
+  ({ hdbCore }) =>
   async (request) => {
     request.body = {
       operation: 'sql',
-      sql: 'SELECT name,id FROM data.libraries ORDER BY name',
+      sql: qb.buildGetQuery('data.libraries', ['name', 'id'], {
+        where: {
+          user: request.jwt.aud,
+        },
+        orderBy: 'name',
+      }),
     };
     return hdbCore.requestWithoutAuthentication(request);
   };
 
-module.exports = { getLibrariesHandler };
+const getLibraryHandler =
+  ({ hdbCore }) =>
+  async (request) => {
+    request.body = {
+      operation: 'sql',
+      sql: qb.buildGetQuery('data.libraries', ['name', 'id'], {
+        where: {
+          user: request.jwt.id,
+          id: request.params.id,
+        },
+        limit: 1,
+      }),
+    };
+    return hdbCore.requestWithoutAuthentication(request);
+  };
+
+const addLibraryHandler =
+  ({ hdbCore }) =>
+  async (request) => {
+    const { name } = request.body;
+    const { aud } = request.jwt;
+    request.body = {
+      operation: 'sql',
+      sql: qb.buildInsertQuery('data.libraries', { name, user: aud }),
+    };
+    return hdbCore.requestWithoutAuthentication(request);
+  };
+
+module.exports = { getLibrariesHandler, addLibraryHandler, getLibraryHandler };
--- api/routes/libraries.js
+++ api/routes/libraries.js
@@ -1,11 +1,27 @@
 'use strict';
 
-const { getLibrariesHandler } = require('../helpers/libraries.handler');
+const authHelpers = require('../helpers/auth.helper');
+const libraryHandlers = require('../helpers/libraries.handler');
 
 module.exports = async (server, hdb) => {
   server.route({
     url: '/libraries',
     method: 'GET',
-    handler: getLibrariesHandler(hdb),
+    preValidation: [authHelpers.authenticationCheck(hdb.logger)],
+    handler: libraryHandlers.getLibrariesHandler(hdb),
+  });
+
+  server.route({
+    url: '/libraries',
+    method: 'POST',
+    preValidation: [authHelpers.authenticationCheck(hdb.logger)],
+    handler: libraryHandlers.addLibraryHandler(hdb),
+  });
+
+  server.route({
+    url: '/libraries/:id',
+    method: 'GET',
+    preValidation: [authHelpers.authenticationCheck(hdb.logger)],
+    handler: libraryHandlers.getLibraryHandler(hdb),
   });
 };
