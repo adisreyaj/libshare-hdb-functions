31b371e785175fc5840e45009182e91ceee3fe9b
Adithya Sreyaj <adi.sreyaj@gmail.com>
2022-01-22 12:35:57 +0530

    refactor(auth): move create user route to signup

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,6 +2,8 @@
 
 const authHelpers = require('../helpers/auth-helper');
 const authHandler = require('../helpers/auth-handler');
+const userHelpers = require('../helpers/users-helper');
+const userHandlers = require('../helpers/users-handler');
 
 module.exports = async (server, hdb) => {
   server.route({
@@ -9,5 +11,12 @@ module.exports = async (server, hdb) => {
     method: 'POST',
     preValidation: [authHelpers.validateLoginBody(hdb.logger)],
     handler: authHandler.loginHandler(hdb),
+  });
+
+  server.route({
+    url: '/signup',
+    method: 'POST',
+    preValidation: userHelpers.validateUser(hdb.logger),
+    handler: userHandlers.createUserHandler(hdb),
   });
 };
--- api/routes/users.js
+++ api/routes/users.js
@@ -2,16 +2,8 @@
 
 const authHelpers = require('../helpers/auth-helper');
 const userHandlers = require('../helpers/users-handler');
-const userHelpers = require('../helpers/users-helper');
 
 module.exports = async (server, hdb) => {
-  server.route({
-    url: '/users',
-    method: 'POST',
-    preValidation: userHelpers.validateUser(hdb.logger),
-    handler: userHandlers.createUserHandler(hdb),
-  });
-
   server.route({
     url: '/users/:id',
     method: 'GET',
